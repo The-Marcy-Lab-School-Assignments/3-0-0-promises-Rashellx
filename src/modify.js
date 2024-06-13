66c2b0165fcd7e8f0d4b5555201bd1c1c9d17d3e
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-06-13 15:04:05 -0400

    Done with code

--- src/modify.js
+++ src/modify.js
@@ -1,9 +1,11 @@
 const crypto = require('crypto');
 
+//This is converting the random numbers to colors
 const numsToRGBColor = ([color1, color2, color3]) => {
   return `rgb(${color1}, ${color2}, ${color3})`;
 };
 
+// This is getting random numbers
 const getRandomBytes = () => new Promise((resolve, reject) => {
   crypto.randomFill(new Uint8Array(3), (err, buffer) => {
     if (err) return reject(err);
@@ -11,16 +13,25 @@ const getRandomBytes = () => new Promise((resolve, reject) => {
   });
 });
 
+// This is pushing the pushing the colors into an array by invoking both previous functions
 const return4RandomColors = () => {
   const colors = [];
   return getRandomBytes()
-    .then(() => {
+    .then((buffer) => {
+      colors.push(numsToRGBColor(buffer))
+      return getRandomBytes()
     })
-    .then(() => {
+    .then((buffer) => {
+      colors.push(numsToRGBColor(buffer))
+      return getRandomBytes()
     })
-    .then(() => {
+    .then((buffer) => {
+      colors.push(numsToRGBColor(buffer))
+      return getRandomBytes()
     })
-    .then(() => {
+    .then((buffer) => {
+      colors.push(numsToRGBColor(buffer))
+      return getRandomBytes()
     })
     .catch((err) => {
       console.error(err);
